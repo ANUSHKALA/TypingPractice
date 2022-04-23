2b5b80e622dc004e24a64841f4e63223bc21656e
ANUSHKALA <anushkakaimal2003@gmail.com>
2022-04-23 18:18:18 +0530

    timer works

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,9 +3,9 @@ import Start from './Start';
 
 export default function Header(){
     return(
-        <nav class="navbar navbar-light bg-secondary">
-            <div class="container-fluid">
-                <span class="navbar-brand mb-0 h1" >Typing Practice</span>
+        <nav className="navbar navbar-light bg-secondary">
+            <div className="container-fluid">
+                <span className="navbar-brand mb-0 h1" >Typing Practice</span>
                 < Start />
             </div>
         </nav>
--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -2,14 +2,25 @@ import React from 'react';
 
 export default function Timer(props){
 
-    let time = 90;
+    let time = 10;
 
     function updateTime(){
         document.getElementById('timer').innerHTML = time;
         time--;
+
+        if(time == 0){
+            clearInterval();
+            document.getElementById('timer').innerHTML = "Time Over!!!";
+        }
     }
 
-    setInterval(updateTime,1000);
+    var interv = setInterval(() => {
+        document.getElementById('timer').innerHTML = time;
+        time--;
+        if(time == 0){
+            clearInterval(interv);
+            document.getElementById('timer').innerHTML = "Time Over!!!";
+        }}, 1000);
 
     //document.getElementById('timer').innerHTML = time;
 
